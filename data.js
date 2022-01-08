4644d7bac0fd79a9623b30c83833256359e87509
Abhijith KR <abhijithkr7745@gmail.com>
2022-01-08 21:50:52 +0530

    Completed

---
+++ data.js
@@ -0,0 +1,58 @@
+let songs = [
+    {
+        name: 'Minnal Murali',
+        path: 'musics/Thee-Minnal.mp3',
+        artist: 'Super Hero',
+        cover: 'https://www.pinkvilla.com/imageresize/tovino_thomas_minnal_murali_ott_release_1.jpg?width=752&format=webp&t=pvorg'
+    },
+    {
+        name: 'Minnal Murali',
+        path: 'musics/Kugramame.mp3',
+        artist: 'Super Hero',
+        cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3Jxs4R1NEt56aJGioPMeMcOBD5p-tRLeg6w&usqp=CAU'
+    },
+    {
+        name: 'song 1',
+        path: 'musics/Song 1.mp3',
+        artist: 'artist 1',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'musics/Song 2.mp3',
+        artist: 'artist 2',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'song 3',
+        path: 'musics/Song 3.mp3',
+        artist: 'artist 3',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'song 4',
+        path: 'musics/Song 4.mp3',
+        artist: 'artist 4',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'song 5',
+        path: 'musics/Song 5.mp3',
+        artist: 'artist 5',
+        cover: 'images/cover 5.png'
+    },
+]
+
+
+
+
+
+// let songs = [
+//     {
+//         name: 'song 1',
+//         path: 'music/Thee-Minnal.mp3',
+//         artist: 'artist 1',
+//         cover: 'https://www.pinkvilla.com/imageresize/minnal_murali_trailer.jpg?width=752&format=webp&t=pvorg'
+//     },
+//    ,
+// ]
